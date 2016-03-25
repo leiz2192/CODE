bcfa300fff9c73ba83a59c67f617cb82637c4ef5
leiz2192 <zhanglei2192@126.com>
2016-03-25 22:33:36 +0800

    Exponentiation string version

--- cplusplus/Exponentiation/Exponentiation/Exponentiation.cpp
+++ cplusplus/Exponentiation/Exponentiation/Exponentiation.cpp
@@ -4,6 +4,7 @@
 #include "stdafx.h"
 #include <string>
 #include <iostream>
+#include <iomanip>
 
 //char result[200];
 std::string result; //存R^N的结果
@@ -64,14 +65,10 @@ void cutInsignificantTail(std::string& strR)
         return;
     }
 
-    for (int i = strR.size() - 1; i > index; i--)
-    {
-        if ('0' == strR[i] || '.' == strR[i])
-        {
+    for (int i = strR.size() - 1; i > index; i--) {
+        if ('0' == strR[i] || '.' == strR[i]) {
             strR.erase(i);
-        }
-        else
-        {
+        } else {
             return;
         }
     }
@@ -91,12 +88,20 @@ int countPointIndex(std::string& strR)
     return index;
 }
 
-//删除实数中的小数点
+//删除实数中的小数点, 删除小数点后数开头的0
 void deletePoint(std::string& strR)
 {
     int pos = strR.find(".");
     if (pos != strR.npos) {
-        strR = strR.substr(0, pos) + strR.substr(pos + 1, strR.size() - pos);
+        strR.erase(pos, 1);
+    }
+
+    pos = strR.find_first_not_of("0");
+    if (pos != strR.npos) {
+        strR.erase(0, pos);
+    }
+    else {
+        strR = "0";
     }
 }
 
@@ -105,35 +110,25 @@ int _tmain(int argc, _TCHAR* argv[])
     //R对应的字符串
     std::string strR;
     int N = 0;
-    int i = 0;
-    //记录小数点在实数中从右向左数的第几位，如1.26在第3位，4在第0位
-    int pointIndex = 0;
     while (std::cin >> strR >> N)
     {
         cutInsignificantTail(strR);
-        pointIndex = countPointIndex(strR);
+        //记录小数点在实数中从右向左数的第几位，如1.26在第3位，4在第0位
+        int pointIndex = countPointIndex(strR);
         deletePoint(strR);
 
         result.assign(strR.begin(), strR.end());
-        for (i = 2; i <= N; i++) {
+        for (int i = 2; i <= N; i++) {
             highRealMul(result, strR, result);
         }
 
         int len = result.size();
         if ((len - (pointIndex - 1) * N) < 0) {
-            std::cout << ".";
-            for (i = len - (pointIndex - 1) * N; i < 0; i++) {
-                std::cout << "0";
-            }
+            std::cout << "." << std::string((pointIndex - 1) * N - len, '0') << result;
         }
-        //输出小数点
-        std::cout << result.substr(0, len - (pointIndex - 1) * N) << "." << result.substr(len - (pointIndex - 1) * N + 1, (pointIndex - 1) * N);
-        //for (i = 0; i < len; i++) {
-        //    if (i == len - (pointIndex - 1) * N) {
-        //        std::cout << ".";
-        //    }
-        //    std::cout << result[i];
-        //}
+        else {
+            std::cout << result.substr(0, len - (pointIndex - 1) * N) << "." << result.substr(len - (pointIndex - 1) * N);
+        }
         std::cout << "\n";
     }
     
